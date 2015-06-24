db542da843340a905e5b1fe4cb0ff083ebe466db
WildAndrewLee <andrew@wildandrewlee.com>
2015-06-23 20:26:10 -0400

    not connects to live remote db (i don't expect this to affect anythin really)

--- models.py
+++ models.py
@@ -6,5 +6,5 @@ from secrets import db
 Database Connection via SQLAlchemy
 '''
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{0}:{1}@127.0.0.1:3306/{2}'.format(db['username'], db['password'], db['name'])
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{0}:{1}@reticent.io:3306/{2}'.format(db['username'], db['password'], db['name'])
 db = SQLAlchemy(app)
